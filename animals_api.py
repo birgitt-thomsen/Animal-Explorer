385dbccf66b84c513e8414de89d9f4d2fd7ab008
Birgitt Thomsen <birgitt.thomsen@t-online.de>
2026-06-04 23:20:39 +0200

    add parameter to allow user input searches

--- animals_api.py
+++ animals_api.py
@@ -1,13 +1,13 @@
 import requests
 
 API_KEY = "QKBRfsocc4sCpgWU4ynUcog2zTJwM7RmSs6JIAzz"
-SEARCH_TERM = "fox"
 REQUEST_URL = "https://api.api-ninjas.com/v1/animals"
 
-def extract_animals_data():
+
+def extract_animals_data(search_term):
     try:
         headers = {'x-api-key': API_KEY}
-        params = {'name': SEARCH_TERM}
+        params = {'name': search_term}
         response = requests.get(REQUEST_URL, headers=headers, params=params)
         animals = response.json()
         return animals
@@ -26,6 +26,3 @@ def extract_animals_data():
             print(f"HTTP Error {status_code}")
     except requests.exceptions.RequestException as e:
         print(f"Unexpected error: {e}")
-
-
-
